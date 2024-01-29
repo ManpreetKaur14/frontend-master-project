5a855aa87703b9089e3decd112b8147e73f5f48f
Manpreet Kaur <manpreetkaur0699@gmail.com>
2024-01-30 00:42:50 +0530

    Calculator

--- script.js
+++ script.js
@@ -1,21 +1,183 @@
-let charr = '🗣️';
-let rep = 5;
-let result = "";
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".screen");
 
-for(let i = 0; i <rep; i++)
-{
-  result = result + charr;
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
 }
-console.log(result)
 
-const person = {
-  name: "Brian Holt",
-  city: "Seattle",
-  state: "WA",
-  favoriteFood: "🌮",
-  wantsTacosRightNow: true,
-  numberOfTacosWanted: 100,
-};
-console.log(person);
-console.log(person.name);
-console.log(person["name"]);
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleMath(value) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(value);
+      break;
+  }
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
+
+// let buffer = '0' ;
+// let runningTotal = 0;
+// let previousOperator;
+// const screen = document.querySelector('.screen');
+// function buttonClick(value){
+//   if (isNaN(parseInt(value))) {
+//     handleSymbol(value);
+//   } else {
+//     handleNumber(value);
+//   }
+//   rerender();
+// }
+
+// function handleNumber(number){
+//   if (buffer === '0') {
+//     buffer = number;
+//   } else {
+//     buffer += number;
+//   }
+// console.log(buffer);
+// }
+
+// function handleMath(value){
+//   if (buffer === '0'){
+//     return;
+//   }
+//   const intBuffer = parseInt(buffer);
+//   if(runningTotal === 0){
+//     runningTotal = intBuffer;
+//   } else {
+//     flushOperation(intBuffer);
+//   }
+
+//   previousOperator = value;
+//   buffer = '0';
+//   console.log(runningTotal)
+// }
+
+
+// function flushOperation(intBuffer) {
+//   if (previousOperator === "+") {
+//     runningTotal += intBuffer;
+//   } else if (previousOperator === "-") {
+//     runningTotal -= intBuffer;
+//   } else if (previousOperator === "×") {
+//     runningTotal *= intBuffer;
+//   } else {
+//     runningTotal /= intBuffer;
+//   }
+// }
+
+
+// function handleSymbol(symbol){
+//   switch (symbol) {
+//     case "C":
+//       buffer = "0";
+//       break;
+//     case "=":
+//       break;
+//     case "←":
+//       if (buffer.length === 1){
+//         buffer = "0";
+//       } else 
+//          buffer = buffer.substring(0, buffer.length - 1);
+//       break;
+//     case "+":
+//     case "-":
+//     case "×":
+//     case "÷":
+//       break;
+//   }
+// }
+
+// function rerender() {
+//   screen.innerText = buffer;
+// }
+
+// function init() {
+//   document
+//   .querySelector(".calc-buttons")
+//   .addEventListener("click", function (event) {
+//     buttonClick(event.target.innerText);
+//   });
+// }
+
+// init();
